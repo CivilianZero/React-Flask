c1f11c2a93fa3b1b3e629ae9bab70851891ed136
Kevin Smith <civilianzero@icloud.com>
2021-01-18 13:56:37 -0500

    removed extra files, fixed duplicate login screen bug, ran linter/formatter again to double check spacing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import { MuiThemeProvider } from "@material-ui/core";
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import "./App.css";
+import { MuiThemeProvider } from '@material-ui/core';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
 import SignupInPage from './pages/SignupIn';
-import { theme } from "./themes/theme";
+import { theme } from './themes/theme';
 
 function App() {
-	return (
-		<MuiThemeProvider theme={theme}>
-			<BrowserRouter>
-				<Route path={["/", "/signup"]} component={SignupInPage} />
-				<Route exact path="/login" component={SignupInPage} />
-			</BrowserRouter>
-		</MuiThemeProvider>
-	);
+  return (
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Route exact path={['/', '/signup']} component={SignupInPage} />
+        <Route exact path='/login' component={SignupInPage} />
+      </BrowserRouter>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
