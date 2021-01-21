bce4d64f5d483374097db1f7c0f9f1f0b6bc71f5
Kevin Smith <civilianzero@icloud.com>
2021-01-21 15:25:39 -0500

    refactoring/formatting, renamed pages with more explicit names, start setup for main messaging page

---
+++ client/src/pages/MessagingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const MessagingPage = () => {
+
+	return (
+		<p>This is messaging page</p>
+	);
+};
+
+export default withRouter(MessagingPage);
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -1,15 +1,15 @@
-import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme } from '@material-ui/core';
 
 export const theme = createMuiTheme({
 	typography: {
-		fontFamily: '"Open Sans"'
+		fontFamily: '"Open Sans"',
 	},
 	palette: {
 		primary: {
-			main: "#3A8DFF"
+			main: '#3A8DFF',
 		},
 		// secondary: "#1f1f1f",
 		// error: "#d8000c",
 		// bgcolor: "#f6f6f6"
-	}
+	},
 });
